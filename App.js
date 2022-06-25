47564c8672d5efc4ffa20aeafd4817ca0d9aa342
RHsanto <rakibulsanto789@gmail.com>
2022-06-25 15:26:11 +0600

    add font

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { typography } from './src/theme/typography';
+
+
+
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    GraphikExtralight: require('./assets/fonts/GraphikExtralight.otf'),
+    GraphikBlack: require('./assets/fonts/GraphikBlack.otf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text  style={{ fontSize: 30,fontFamily:typography.primary }}>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily:'GraphikExtralight',fontSize: 30,}}>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/theme/typography.js
@@ -0,0 +1,4 @@
+export const typography ={
+  primary:'GraphikExtralight',
+  primaryBold:'GraphikBlack'
+}
