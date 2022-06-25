6499fa53170dd3cfeeec5c5b768958f595916e34
RHsanto <rakibulsanto789@gmail.com>
2022-06-25 13:08:10 +0600

    add colors

---
+++ src/theme/color.js
@@ -0,0 +1,10 @@
+export const colors={
+  black:'#000',
+  white:'#fff',
+  violet:'#473F97',
+  blue:': #4D79FF',
+  orange:'#FF4D58',
+  pink:' #FFA6B9',
+  violet1:'#56549E',
+  violet2:'  #AEA1E5'
+}
