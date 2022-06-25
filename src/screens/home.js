c0887585ec97384d743a1643b2ec183fc894f3b2
RHsanto <rakibulsanto789@gmail.com>
2022-06-25 16:11:00 +0600

    add naivigation

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,13 +1,35 @@
-import { View } from "react-native";
+import { Image, SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
 import Text from "../components/text/text";
+import { colors } from "../theme/color";
 
 
 export default function Home(){
 
   return(
-    <View>
-      <Text>Hello Home</Text>
-    </View>
+   <SafeAreaView>
+    <ScrollView>
+      <View style={styles.firstContainer}>
+      <View style={styles.navbar}>
+        <View>
+          <Image source={require('../../assets/image/menu.png')} />
+        </View>
+        <View></View>
+      </View>
+      </View>
+    </ScrollView>
+   </SafeAreaView>
   )
 
 }
+const styles = StyleSheet.create({
+  firstContainer:{
+    backgroundColor:colors.violet,
+    borderBottomLeftRadius:30,
+    borderBottomRightRadius:30,
+    
+  },
+  navbar:{
+    alignItems:'center',
+    justifyContent:'space-between'
+  }
+})
