a771ae6a8f1fc112b04b9f9ae2eba2e11c34a958
RHsanto <rakibulsanto789@gmail.com>
2022-06-25 15:46:01 +0600

    done common componnts text

--- src/components/text/text.js
+++ src/components/text/text.js
@@ -3,7 +3,7 @@ import { presets } from "./text.preset";
 
 
 export default function Text({children,preset: preset="default",style}){
- 
+//  এখানে আমাদের preset ও react-native এর style এক করা হয়েছে 
   const textStyle =StyleSheet.compose(presets[preset],style); 
   return<RNtext style={textStyle}>{children}</RNtext>
 }
